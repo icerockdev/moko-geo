120fcb0c2f0ad7f9f072dc8a310999f634a92893
Anton Shestak <ashestak@icerockdev.com>
2021-06-29 18:34:15 +0700

    #24 to convention plugin

--- sample/android-app/build.gradle.kts
+++ sample/android-app/build.gradle.kts
@@ -5,39 +5,14 @@ plugins {
 }
 
 android {
-    compileSdkVersion(libs.versions.compileSdk.get().toInt())
-
     buildFeatures.dataBinding = true
 
-    dexOptions {
-        javaMaxHeapSize = "2g"
-    }
-
     defaultConfig {
-        minSdkVersion(libs.versions.minSdk.get().toInt())
-        targetSdkVersion(libs.versions.targetSdk.get().toInt())
 
         applicationId = "dev.icerock.moko.samples.geo"
 
         versionCode = 1
         versionName = "0.1.0"
-
-        vectorDrawables.useSupportLibrary = true
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-        getByName("debug") {
-            isDebuggable = true
-            applicationIdSuffix = ".debug"
-        }
-    }
-
-    packagingOptions {
-        exclude("META-INF/*.kotlin_module")
     }
 }
 
